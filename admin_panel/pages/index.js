fde08f638068626c6434331c2001371df843960d
fahimabid45 <shoumiksust@gmail.com>
2025-03-19 14:04:52 +0600

    first

---
+++ admin_panel/pages/index.js
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    
+    <div className="w-screen h-screen flex items-center bg-blue-600">
+      <div className="text-center w-full">
+        <button className="bg-white p-2 rounded-md">Login</button>
+      </div>
+      
+    </div>
+  );
+}
