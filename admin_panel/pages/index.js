ef707bb0c675333a7318ce4f849fe72f78b36a5b
sifat <sifatsust14@gmail.com>
2025-03-22 01:39:29 -0700

    feat: layout feature

--- admin_panel/pages/index.js
+++ admin_panel/pages/index.js
@@ -1,23 +1,7 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import Nav from "@/components/Nav";
+
+import Layout from "@/components/Layout";
 export default function Home() {
-  const { data: session } = useSession()
-  if(!session)
-  {return (
-    
-    <div className="w-screen h-screen flex items-center bg-blue-600">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 rounded-md" onClick={()=>signIn('google')}>Login with google</button>
-      </div>
-      
-    </div>
-  )}
-  return (
-    
-    <div className="bg-blue-600 w-screen h-screen">
-      <Nav />
-     logged in {session.user.name}
-      
-    </div>
-  );
+  return <Layout>
+    test
+  </Layout>
 }
