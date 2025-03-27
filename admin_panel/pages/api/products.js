3e1bb8f002d6f2f068a8f580e5060c2364a03802
sifat <sifatsust14@gmail.com>
2025-03-27 12:29:48 -0700

    feat:Adding Deletetion

--- admin_panel/pages/api/products.js
+++ admin_panel/pages/api/products.js
@@ -32,4 +32,12 @@ export default async function handle(req,res)
         await Product.updateOne({_id},{name,about,price})
         res.json(true)
     }
+    if(method==='DELETE')
+    {
+        if(req.query?.id)
+        {
+            await Product.deleteOne({_id:req.query?.id})
+            res.json(true)
+        }
+    }
  }
