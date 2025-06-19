2347c08dfbb7ff673d321e48c9f617a639520317
fahimabid45 <shoumiksust@gmail.com>
2025-06-19 18:13:16 +0600

    Adding Admin Security

--- admin_panel/components/Layout.js
+++ admin_panel/components/Layout.js
@@ -1,28 +1,36 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn } from "next-auth/react";
 import Nav from "@/components/Nav";
-export default function Layout({children}) {
-  const { data: session } = useSession()
-  if(!session)
-  {return (
-    
-   
-    <div className="w-screen h-screen flex items-center ">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 rounded-md" onClick={()=>signIn('google')}>Login with google</button>
+
+export default function Layout({ children }) {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <div className="w-screen h-screen flex items-center justify-center">
+        <p className="text-white text-xl">Loading...</p>
       </div>
-      
-    </div>
-  )}
+    );
+  }
+
+  if (!session) {
+    return (
+      <div className="w-screen h-screen flex items-center justify-center">
+        <button
+          className="bg-white p-3 px-5 rounded-md text-black"
+          onClick={() => signIn("google")}
+        >
+          Login with Google
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="bg-blue-900 min-h-screen flex">
-    <Nav />
-    <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
-
-    
-    {children}
+      <Nav />
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+        {children}
+      </div>
     </div>
-  
-    
-  </div>
   );
 }
--- admin_panel/components/Nav.js
+++ admin_panel/components/Nav.js
@@ -1,3 +1,4 @@
+import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -46,6 +47,19 @@ export default function Nav()
 
          Categories
          </Link>
+        <button onClick={()=>signOut(
+  { callbackUrl: '/' }
+)
+}  
+className={inactiveLink}>
+            
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+
+         Logout
+         
+         </button>
       </nav>
        </aside>
     )
